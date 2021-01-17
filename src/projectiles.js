bf181dd8cf5f62720cf96c109c7126ec5700ef40
dlbjames <dljames@uw.edu>
2021-01-17 02:51:14 -0800

    Added total and level timers. Removed unused variables from player and bullet classes

--- src/projectiles.js
+++ src/projectiles.js
@@ -1,6 +1,10 @@
 class PlayerBullet {
-    constructor(game, x, y, size, playerX, playerY) {
-        Object.assign(this, {game, x, y, size, playerX, playerY});
+    constructor(game, x, y, size) {
+        Object.assign(this, {game, x, y, size});
+
+        // Use these variables to stop bullets from colliding with player
+        this.playerX = this.x;
+        this.playerY = this.y;
 
         // Use these variables to define the player bullet
         this.width = 10;
@@ -20,7 +24,7 @@ class PlayerBullet {
         this.y -= this.bulletSpeed;
 
         // If the bullet leaves the screen remove it 
-        if (this.y > 675 || this.y < 0 || (this.x == this.playerX && this.y == this.playerY)) {
+        if (this.y > 740 || this.y < 0 || (this.x == this.playerX && this.y == this.playerY)) {
             this.removeFromWorld = true;
         }
     }
@@ -28,8 +32,11 @@ class PlayerBullet {
 
 
 class AltPlayerBullet {
-    constructor(game, x, y, size, playerX, playerY) {
-        Object.assign(this, {game, x, y, size, playerX, playerY});
+    constructor(game, x, y, size) {
+        Object.assign(this, {game, x, y, size});
+
+        this.playerX = this.x;
+        this.playerY = this.y;
 
         this.width = 10;
         this.height = 30;
