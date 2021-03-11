34baaee717655c77ea61d278c25d521f8c0b2271
Anthony <nguyenanthony80@gmail.com>
2021-03-10 22:25:22 -0800

    fixed bg stacking when dying

--- src/engine/gameengine.js
+++ src/engine/gameengine.js
@@ -50,6 +50,7 @@ class GameEngine {
 
     // TODO - make reset dynamic based upon current level. switch statement ? Or ?
     reset() {
+        this.entities.level.bgMusic.pause();
         this.entities = [];
         this.score = 0;
         this.displayScore.innerHTML = 0;
